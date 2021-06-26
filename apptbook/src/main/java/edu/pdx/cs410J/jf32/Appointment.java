3fa6b12a435db9025506b223d411bb5147399528
Joe.Fletcher <31193255+JFletch3@users.noreply.github.com>
2021-06-26 15:23:47 -0700

    Update Appointment.java

--- apptbook/src/main/java/edu/pdx/cs410J/jf32/Appointment.java
+++ apptbook/src/main/java/edu/pdx/cs410J/jf32/Appointment.java
@@ -3,18 +3,48 @@ package edu.pdx.cs410J.jf32;
 import edu.pdx.cs410J.AbstractAppointment;
 
 public class Appointment extends AbstractAppointment {
-  @Override
-  public String getBeginTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+
+  private String description;
+  private String startTime;
+  private String endTime;
+
+//  public Appointment(String desc, String start, String end)
+//  {
+//    description = desc;
+//    startTime = start;
+//    endTime = end;
+//  }
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+
+  public void setStartTime(String startTime)
+  {
+    this.startTime = startTime;
+  }
+
+  public void setEndTime(String endTime)
+  {
+    this.endTime = endTime;
   }
 
   @Override
-  public String getEndTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+  public String getBeginTimeString()
+  {
+    return startTime;
   }
 
   @Override
-  public String getDescription() {
-    return "This method is not implemented yet";
+  public String getEndTimeString()
+  {
+    //throw new UnsupportedOperationException("This method is not implemented yet");
+    return endTime;
+  }
+
+  @Override
+  public String getDescription()
+  {
+    return description;
   }
 }
