b994d98e9807fb126b658ac928cf24ab188ed010
Joe.Fletcher <31193255+JFletch3@users.noreply.github.com>
2021-06-28 13:02:52 -0700

    Update Project1.java

--- apptbook/src/main/java/edu/pdx/cs410J/jf32/Project1.java
+++ apptbook/src/main/java/edu/pdx/cs410J/jf32/Project1.java
@@ -9,24 +9,24 @@ public class Project1 {
 
   public static void main(String[] args) {
 
-//    Collection<Appointment> appts;
-//    AppointmentBook newAppointmentBook = new AppointmentBook(args[0]);
-//    Appointment appointment = new Appointment();
-//
-//    appointment.setDescription(args[1]);
-//    appointment.setStartTime(args[2], args[3]);
-//    appointment.setEndTime(args[4], args[5]);
-//
-//    newAppointmentBook.addAppointment(appointment);
-//
-//    appts = newAppointmentBook.getAppointments();
-//
-//    System.out.println(newAppointmentBook);
-//    for(Appointment ap : appts)
-//    {
-//      System.out.println(ap.toString());
-//    }
-    System.out.println("testing program");
+    Collection<Appointment> appts;
+    AppointmentBook newAppointmentBook = new AppointmentBook(args[0]);
+    Appointment appointment = new Appointment();
+
+    appointment.setDescription(args[1]);
+    appointment.setStartTime(args[2], args[3]);
+    appointment.setEndTime(args[4], args[5]);
+
+    newAppointmentBook.addAppointment(appointment);
+
+    appts = newAppointmentBook.getAppointments();
+
+    System.out.println(newAppointmentBook);
+    for(Appointment ap : appts)
+    {
+      System.out.println(ap.toString());
+    }
+
 
 //    for (String arg : args) {
 //      System.out.println(arg);
