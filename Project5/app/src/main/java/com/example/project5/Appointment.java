d54be016653dc2c5893e86b572ca0b70358b9a02
Joe.Fletcher <31193255+JFletch3@users.noreply.github.com>
2021-07-28 19:04:54 -0700

    p5 work

---
+++ Project5/app/src/main/java/com/example/project5/Appointment.java
@@ -0,0 +1,26 @@
+package com.example.project5;
+
+import edu.pdx.cs410J.AbstractAppointment;
+
+public class Appointment extends AbstractAppointment
+{
+
+
+    @Override
+    public String getBeginTimeString()
+    {
+        return null;
+    }
+
+    @Override
+    public String getEndTimeString()
+    {
+        return null;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return null;
+    }
+}
